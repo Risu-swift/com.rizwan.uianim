5da409ca28542577ad00b9a37fa52e3f0cc7f611
Rizwan <redrizwan007@gmail.com>
2024-08-01 13:44:07 +0400

    Added Animation

--- Runtime/ImageAnimation.cs
+++ Runtime/ImageAnimation.cs
@@ -7,18 +7,115 @@ using UnityEngine.UI;
 public class ImageAnimation : MonoBehaviour
 {
     [SerializeField] private ImageAnimSequence animationSequence;
+    [SerializeField] private bool isAutoPlay = false;
+    [SerializeField] private bool isLoop = false;
 
     private Image image;
-    private int currentFrame = 0;
+
+    private float frameDuration;
+    private int totalFrames;
+    private bool isPlaying = false;
+    private float playbackPercentage = 1f;
+    private float elapsedTime;
+    private int startFrameIndex;
+    private int endFrameIndex;
+    private bool hasStarted = false;
+
+
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         image = gameObject.GetComponent<Image>();
+
+        if (animationSequence != null)
+        {
+            frameDuration = 1f / animationSequence.TargetFPS;
+            totalFrames = animationSequence.AnimationSequence.Count;
+
+            if (isAutoPlay)
+            {
+                Play(1f);
+            }
+        }
+
     }
+
+
+    public void PlayPercent(int percentage) => Play((float)percentage / 100f);
 
     // Update is called once per frame
     void Update()
     {
+        if (isPlaying)
+        {
+            elapsedTime += Time.deltaTime;
 
+            if (elapsedTime >= frameDuration)
+            {
+                elapsedTime = 0f;
+
+                if (hasStarted)
+                {
+                    startFrameIndex++;
+
+                    if (startFrameIndex > endFrameIndex)
+                    {
+                        if (isLoop)
+                        {
+                            startFrameIndex = Mathf.FloorToInt(totalFrames * 0);
+                            elapsedTime = 0f;
+                        }
+                        else
+                        {
+                            isPlaying = false;
+                            startFrameIndex = endFrameIndex;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                hasStarted = true;
+            }
+
+            if (hasStarted)
+            {
+                image.sprite = animationSequence.AnimationSequence[startFrameIndex];
+            }
+        }
+    }
+
+    public void Play(float percentage)
+    {
+        if (animationSequence == null || percentage <= 0f) return;
+
+        if (!isPlaying)
+        {
+            elapsedTime = 0f;
+            hasStarted = false;
+            isPlaying = true;
+        }
+
+        playbackPercentage = Mathf.Clamp01(percentage);
+
+        int currentFrameIndex = animationSequence.AnimationSequence.IndexOf(image.sprite);
+
+        if (currentFrameIndex < 0)
+        {
+            currentFrameIndex = 0;
+        }
+
+        startFrameIndex = currentFrameIndex;
+        endFrameIndex = Mathf.FloorToInt(totalFrames * playbackPercentage) - 1;
+
+        if (endFrameIndex >= totalFrames) endFrameIndex = totalFrames - 1;
+
+        image.sprite = animationSequence.AnimationSequence[startFrameIndex];
+    }
+
+    private void ShowFrame(int frameNumber)
+    {
+        image.sprite = animationSequence.AnimationSequence[frameNumber];
+        // currentFrame++;
     }
 }
